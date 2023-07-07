22e98fdcef225a6c3b50ea71594b93f03a28a32c
Conner A <eraserlark@gmail.com>
2023-07-07 13:53:27 -0500

    Title Screen

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //Connect to first demo scene
+    }
+
+    public void OpenCredits()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+
+    }
+}
