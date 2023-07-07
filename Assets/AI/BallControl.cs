eabdb8dbbf86a299919e5198599db0cab9bc3e33
rucalder <60902416+rubencal99@users.noreply.github.com>
2023-07-07 13:57:49 -0500

    More basic visuals

---
+++ Assets/AI/BallControl.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script allows players to take posession of ball and maneuver it
+public class BallControl : MonoBehaviour
+{
+    
+}
