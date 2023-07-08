f1bc302daacce2d8bb6fd4e6562c142dde5e333a
Conner A <eraserlark@gmail.com>
2023-07-08 09:48:35 -0500

    Basic team data

---
+++ Assets/Scripts/Stage/PlayerTeam.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class PlayerTeam : MonoBehaviour
+{
+    [SerializeField]
+    private Team playerTeam;
+}
---
+++ Assets/Scripts/Teams/TeamManager.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamManager : MonoBehaviour
+{
+    public static TeamManager instance;
+    public List<Team> teamsList;
+}
