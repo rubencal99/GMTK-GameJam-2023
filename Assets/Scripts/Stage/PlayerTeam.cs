1fab54d25fb236238dd9e618c86a9cb24cb08e41
Conner A <eraserlark@gmail.com>
2023-07-08 11:44:36 -0500

    Player Shader for team colors

--- Assets/Scripts/Stage/PlayerTeam.cs
+++ Assets/Scripts/Stage/PlayerTeam.cs
@@ -3,5 +3,25 @@ using UnityEngine;
 public class PlayerTeam : MonoBehaviour
 {
     [SerializeField]
-    private Team playerTeam;
+    private GameObject avatar;
+
+    [SerializeField]
+    private Team team;
+    //public Team playerTeam
+    //{
+    //    get
+    //    {
+    //        return team;
+    //    }
+    //    set
+    //    {
+    //        team = value;
+    //        avatar.GetComponent<Renderer>().sharedMaterial.SetColor("_UniformColor", team.color);
+    //    }
+    //}
+
+    private void Awake()
+    {
+        avatar.GetComponent<Renderer>().sharedMaterial.SetColor("_UniformColor", team.color);
+    }
 }
